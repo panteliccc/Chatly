0754a59b4f4bb18140956e9dcc361d55cbb2ed7c
Nikola Pantelic <pantelicnikolaa@gamail.com>
2024-04-29 14:34:07 +0200

    add emoji

--- client/src/Components/Messages/sendMessage.tsx
+++ client/src/Components/Messages/sendMessage.tsx
@@ -3,6 +3,10 @@ import { Input } from "../ui/input";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { useChatState } from "../../Context/Provider";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceSmile } from "@fortawesome/free-regular-svg-icons";
 interface Message {
   user: User;
   text: string;
@@ -16,8 +20,8 @@ interface User {
   image: string;
   isDeleted: boolean;
 }
-interface Props{
-  socket:any;
+interface Props {
+  socket: any;
 }
 function SendMessage(props: Props) {
   const chatState = useChatState();
@@ -41,11 +45,11 @@ function SendMessage(props: Props) {
           withCredentials: true,
         }
       );
-      props.socket.emit('new message', data)
+      props.socket.emit("new message", data);
       chatState.setMessages((prev: Message[]) => {
         return [...prev, data];
       });
-      chatState.setRefreshChats(!chatState.refreshChats)
+      chatState.setRefreshChats(!chatState.refreshChats);
       setMessage("");
     } catch (err) {
       console.log(err);
@@ -55,19 +59,41 @@ function SendMessage(props: Props) {
   const onKeyDown = (e: any) => {
     if (e.key === "Enter") sendMessage();
   };
+  const [showPicker, setShowPicker] = useState(false);
+
+  const onEmojiClick = (emoji: any) => {
+    setMessage((prevInput) => prevInput + emoji.native);
+    setShowPicker(false);
+  };
 
   return (
-    <div className="p-3 bg-primary flex gap-3">
-      <Input
-        placeholder="Message..."
-        className=" rounded bg-secondary py-7 px-3 shadow-none focus-visible:ring-0 border-0"
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-        onKeyDown={(e) => {
-          onKeyDown(e);
-        }}
-      />
-
+    <div className="p-3 bg-primary flex gap-3 relative">
+      <div className="rounded bg-secondary w-full flex px-2 items-center">
+        {showPicker && (
+          <div className=" absolute bottom-24 left-0 w-full md:w-auto md:left-3">
+            <Picker
+              data={data}
+              onEmojiSelect={(e: any) => {
+                onEmojiClick(e);
+              }}
+            />
+          </div>
+        )}
+        <FontAwesomeIcon
+          icon={faFaceSmile}
+          onClick={() => setShowPicker((val) => !val)}
+          className="emoji-icon text-2xl cursor-pointer"
+        />
+        <Input
+          placeholder="Message..."
+          className="py-7 px-3 shadow-none focus-visible:ring-0 border-0 w-full break-words "
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          onKeyDown={(e) => {
+            onKeyDown(e);
+          }}
+        />
+      </div>
       <button
         className="w-16 bg-softBlue md:flex items-center justify-center rounded hidden"
         onClick={sendMessage}
